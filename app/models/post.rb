6d5ee4f13c16997f929e18dc8983cedb35d1b1ea
kaburagi-hideaki <kaburagi4649@gmail.com>
2019-12-03 14:15:11 +0900

    登録記事の登録機能完成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+  attribute :new_image
+
+  before_save do
+    if new_image
+      self.image = new_image if new_image
+    end
+  end
 end
